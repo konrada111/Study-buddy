5b9cfa5c9f6862baee2bfa9211157420264cb9e5
konrada111 <konrada111@gmail.com>
2021-02-18 20:05:56 +0100

    UsersList

---
+++ src/data/users.js
@@ -0,0 +1,31 @@
+export const users = [
+  {
+    name: 'Adam Romański',
+    attendance: '40%',
+    average: '2.3',
+  },
+  {
+    name: 'Krzysztof Batko',
+    attendance: '38%',
+    average: '2.3',
+  },
+  {
+    name: 'Patrycja Marchlewska',
+    attendance: '45%',
+    average: '3.5',
+  },
+  {
+    name: 'Mariusz Pudzianowski',
+    attendance: '60%',
+    average: '4.5',
+  },
+  {
+    name: 'Mariola Mankowska',
+    attendance: '56%',
+    average: '5.0',
+  },
+  {
+    name: 'Marian Kowska',
+    average: '5.0',
+  },
+];
