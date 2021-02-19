bc84ea97857bfdedd57209c3a01e3f5bbbdbb5a4
konrada111 <konrada111@gmail.com>
2021-02-19 23:38:24 +0100

    style list element

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { users } from 'data/users.js';
 import UsersListItem from 'components/UsersListItem/UsersListItem.js';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-color: #f7f8fa;
+  width: 100%;
+  max-width: 500px;
+  padding: 40px 30px;
+  border-radius: 25px;
+  box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.7);
+`;
 
 const UsersList = () => (
-  <div>
+  <Wrapper>
     <ul>
       {users.map((userData) => (
         <UsersListItem userData={userData} />
       ))}
     </ul>
-  </div>
+  </Wrapper>
 );
 
 export default UsersList;
--- src/components/UsersListItem/UsersListItem.js
+++ src/components/UsersListItem/UsersListItem.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.li`
+  display: flex;
+  align-items: center;
+  position: relative;
+  &:not(:last-child)::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    background-color: lightgray;
+  }
+`;
+
+const StyledButton = styled.button`
+  width: 25px;
+  height: 25px;
+  background-color: ${({ isSecondary }) => (isSecondary ? '#e7e044' : '#c0c7d6')};
+  border-radius: ${({ isSquared }) => (isSquared ? '0px' : '50px')};
+  border: none;
+  color: white;
+`;
 
 const UserListItem = ({ userData: { name, average, attendance = '0%' } }) => (
-  <div>
-    <ul>
-      <li>
-        <div>{average}</div>
-        <div>
-          <p>{name}</p>
-          <p>Attendance: {attendance}</p>
-        </div>
-        <button>X</button>
-      </li>
-    </ul>
-  </div>
+  <Wrapper>
+    <div>{average}</div>
+    <div>
+      <p>{name}</p>
+      <p>Attendance: {attendance}</p>
+    </div>
+    <StyledButton>X</StyledButton>
+    <StyledButton isSecondary isSquared>
+      X
+    </StyledButton>
+  </Wrapper>
 );
 
 UserListItem.propTypes = {
