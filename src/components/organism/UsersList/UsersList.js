3656bfa9dc76afeb4ad601420cbc41513a253ff3
konrada111 <konrada111@gmail.com>
2021-02-23 22:44:59 +0100

    add button state

--- src/components/organism/UsersList/UsersList.js
+++ src/components/organism/UsersList/UsersList.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { users } from 'data/users.js';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem.js';
-import { Wrapper } from './UsersList.style';
+import { StyledList, Wrapper } from './UsersList.style';
 
-const UsersList = () => (
-  <Wrapper>
-    <ul>
-      {users.map((userData) => (
-        <UsersListItem key={userData.name} userData={userData} />
-      ))}
-    </ul>
-  </Wrapper>
-);
+class UsersList extends React.Component {
+  state = {
+    isUsersList: true,
+  };
+
+  toggleListTitle = () => {
+    this.setState((prevState) => ({
+      isUsersList: !prevState.isUsersList,
+    }));
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <h1>{this.state.isUsersList ? `User's List` : `Student's List`}</h1>
+        <button onClick={this.toggleListTitle}>Change tittle</button>
+        <StyledList>
+          {users.map((userData) => (
+            <UsersListItem key={userData.name} userData={userData} />
+          ))}
+        </StyledList>
+      </Wrapper>
+    );
+  }
+}
 
 export default UsersList;
