254a328ccf85cf465d7e26d46100b8824c15cbee
konrada111 <konrada111@gmail.com>
2021-02-26 21:24:07 +0100

    add 3 stages of lifestyle components

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import Button from 'components/atoms/Button/Button';
 import { Wrapper, StyledAverage, StyledInfo } from './UsersListItem.style';
 
-const UserListItem = ({ userData: { name, average, attendance = '0%' } }) => (
+const UserListItem = ({ deleteUser, userData: { name, average, attendance = '0%' } }) => (
   <Wrapper>
     <StyledAverage averageValue={average}>{average}</StyledAverage>
     <StyledInfo>
       <p>{name}</p>
       <p>Attendance: {attendance}</p>
     </StyledInfo>
-    <Button />
+    <Button onClick={() => deleteUser(name)} />
   </Wrapper>
 );
 
--- src/components/organism/UsersList/UsersList.js
+++ src/components/organism/UsersList/UsersList.js
@@ -5,8 +5,18 @@ import { StyledList, Wrapper } from './UsersList.style';
 
 class UsersList extends React.Component {
   state = {
+    users,
     isUsersList: true,
   };
+
+  //odpala sie kiedy nasz komponent zostanie zamontowany w aplikacji(zostaje wyrenderowany po raz pierwszy)
+  componentDidMount() {}
+
+  //odpala sie kiedy doszlo do jakiegos update-u (mozemy porownywac poprzednie propsy, poprzednie stany oraz jego cala konstrukcje ktora zawieral
+  componentDidUpdate(prevProps, prevState, snapshot) {}
+
+  //uruchamia sie w momencie kiedy jest odmontowany od naszego domu
+  componentWillUnmount() {}
 
   toggleListTitle = () => {
     this.setState((prevState) => ({
@@ -14,14 +24,19 @@ class UsersList extends React.Component {
     }));
   };
 
+  deleteUser = (name) => {
+    const filteredUsers = this.state.users.filter((user) => user.name !== name);
+    this.setState({ users: filteredUsers });
+  };
+
   render() {
     return (
       <Wrapper>
-        <h1>{this.state.isUsersList ? `User's List` : `Student's List`}</h1>
-        <button onClick={this.toggleListTitle}>Change tittle</button>
+        <h1>{this.state.isUsersList ? `Users List` : `Students List`}</h1>
+        <button onClick={() => this.toggleListTitle(this.state.isUsersList)}>Change title</button>
         <StyledList>
-          {users.map((userData) => (
-            <UsersListItem key={userData.name} userData={userData} />
+          {this.state.users.map((userData) => (
+            <UsersListItem deleteUser={this.deleteUser} key={userData.name} userData={userData} />
           ))}
         </StyledList>
       </Wrapper>
